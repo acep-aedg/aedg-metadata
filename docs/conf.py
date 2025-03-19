ab485bbe5a4c3e4be19cdb24b110c1736506ccaf
Elizabeth (Liz) Dobbins <eldobbins@alaska.edu>
2025-03-19 10:02:56 -0800

    trying sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+project = "example"
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+]
+source_suffix = [".rst", ".md"]
---
+++ docs/index.md
@@ -0,0 +1,11 @@
+# Example documentation
+
+Hello world!
+
+```{eval-rst}
+.. autosummary::
+    :nosignatures:
+    :toctree: _generated
+
+    aedg_metadata.gen_meta.AedgOemetadata
+```
