6a6d50c724e84b29f63b64ecf2945837d21408e1
Elizabeth (Liz) Dobbins <eldobbins@alaska.edu>
2025-06-28 15:49:07 -0800

    updates from ISER review

--- run_batch.sh
+++ run_batch.sh
@@ -3,14 +3,14 @@
 # each is different so cannot loop simply with names
 
 # raw tables
-aedg_metadata generate lookup_fuelcode_2023-11-08 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
-aedg_metadata generate lookup_primemover_2023-11-08 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
-aedg_metadata generate lookup_plants_2025-03-10 -d raw/dowl --bbox infer --time none --data-dictionary raw_dowl_data_dictionary.csv --save
-aedg_metadata generate lookup_salesreport_2025-03-03 -d raw/dowl --bbox infer --time none --data-dictionary raw_dowl_data_dictionary.csv --save
-aedg_metadata generate lookup_interties_2024-02-23 -d raw/dowl --bbox none --time specify --data-dictionary raw_dowl_data_dictionary.csv --save
-aedg_metadata generate lookup_grids_2025-04-17 -d raw/dowl --bbox none --time specify --data-dictionary raw_dowl_data_dictionary.csv --save
-aedg_metadata generate lookup_operator_2025-03-07 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
-aedg_metadata generate lookup_communities_2024-02-23 -d raw/dowl --bbox infer --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_fuelcode_2023-11-08 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_primemover_2023-11-08 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_plants_2025-03-10 -d raw/dowl --bbox infer --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_salesreport_2025-03-03 -d raw/dowl --bbox infer --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_interties_2024-02-23 -d raw/dowl --bbox none --time specify --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_grids_2025-04-17 -d raw/dowl --bbox none --time specify --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_operator_2025-03-07 -d raw/dowl --bbox none --time none --data-dictionary raw_dowl_data_dictionary.csv --save
+# aedg_metadata generate lookup_communities_2024-02-23 -d raw/dowl --bbox infer --time none --data-dictionary raw_dowl_data_dictionary.csv --save
 
 # normalized tables:
 # final	boroughs.geojson
@@ -37,16 +37,16 @@ aedg_metadata generate lookup_communities_2024-02-23 -d raw/dowl --bbox infer --
 #aedg_metadata generate yearly_generation -d final --bbox infer --time specify --save
 
 # denormalized tables for the data explorer
-# aedg_metadata generate public_capacity -d public --bbox infer --time specify --save
-# aedg_metadata generate public_communities -d public --bbox infer -t none --save
+aedg_metadata generate public_capacity -d public --bbox infer --time specify --save
+aedg_metadata generate public_communities -d public --bbox infer -t none --save
 # aedg_metadata generate public_employment -d public --bbox infer -t specify --save
-# aedg_metadata generate public_fuel_prices -d public --bbox infer -t specify --save
-# aedg_metadata generate public_monthly_generation -d public --bbox infer --save
+aedg_metadata generate public_fuel_prices -d public --bbox infer -t specify --save
+aedg_metadata generate public_monthly_generation -d public --bbox infer --save
 # aedg_metadata generate public_rates -d public --bbox infer -t none --save  # change this!!
 # aedg_metadata generate public_populations_ages_sexes -d public --bbox infer -t specify --save
 # aedg_metadata generate public_taxes -d public --bbox infer -t specify --save
 # aedg_metadata generate public_transportation -d public --bbox infer -t specify --save
-# aedg_metadata generate public_yearly_generation -d public --bbox infer --save
+aedg_metadata generate public_yearly_generation -d public --bbox infer --save
 
 # Gather keywords/topics together into registry file
 # python src/aedg_metadata/scripts/keywords_to_registry.py
